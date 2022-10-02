c82b34530a3987d971aa5fe0d497411b7affdd1f
Aram Panasenco <panasenco@users.noreply.github.com>
2022-10-02 16:58:20 -0600

    Updated example app to work with most recent version of the adapter

--- example-app/svelte.config.js
+++ example-app/svelte.config.js
@@ -1,12 +1,24 @@
-import adapterBrowserExtension from '../adapter-browser-extension.mjs';
+import adapter from 'sveltekit-adapter-browser-extension';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 	kit: {
-		appDir: 'ext',
-		target: '#svelte',
-		adapter: adapterBrowserExtension()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build', // the output directory for the pages of your extension
+			assets: undefined, // the asset output directory is derived from pages if not specified explicitly
+			fallback: undefined, // set to true to output an SPA-like extension
+			manifestVersion: 3, // the version of the automatically generated manifest (Version 3 is required by Chrome).
+            manifest: {
+                // You can customize manifest here
+            }
+        }),
+		appDir: 'ext', // This is important - chrome extensions can't handle the default _app directory name.
+		prerender: {
+			default: true
+		}
 	}
 };
-
-export default config;
